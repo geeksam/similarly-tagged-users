842e2f21196ce5930ad1e6450fe2faacbaa1c02a
Sam Livingston-Gray <geeksam@gmail.com>
2021-05-11 12:08:59 -0700

    switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_11_181339) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
